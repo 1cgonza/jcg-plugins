af35fae9c09a0f3a5b6d4d8ea701c1ae485e453e
Juan Camilo Gonzalez <info@juancgonzalez.com>
2016-12-21 18:29:33 -0500

    use minified css

--- admin/class-jcg-plugins-admin.php
+++ admin/class-jcg-plugins-admin.php
@@ -1,48 +1,9 @@
 <?php
 
-/**
- * The admin-specific functionality of the plugin.
- *
- * @link       http://juancgonzalez.com
- * @since      1.0.0
- *
- * @package    JCG_Plugins
- * @subpackage JCG_Plugins/admin
- */
-
-/**
- * The admin-specific functionality of the plugin.
- *
- * @package    JCG_Plugins
- * @subpackage JCG_Plugins/admin
- * @author     Juan Camilo Gonzalez <info@juancgonzalez.com>
- */
 class JCG_Plugins_Admin {
-  /**
-   * The ID of this plugin.
-   *
-   * @since    1.0.0
-   * @access   private
-   * @var      string    $plugin_name    The ID of this plugin.
-   */
   private $plugin_name;
-
-  /**
-   * The version of this plugin.
-   *
-   * @since    1.0.0
-   * @access   private
-   * @var      string    $version    The current version of this plugin.
-   */
   private $version;
 
-  /**
-   * Initialize the class and set its properties.
-   *
-   * @since    1.0.0
-   * @param      string    $plugin_name       The name of this plugin.
-   * @param      string    $version    The version of this plugin.
-   */
   public function __construct($plugin_name, $version) {
     $this->plugin_name = $plugin_name;
     $this->version = $version;
@@ -77,24 +38,14 @@ class JCG_Plugins_Admin {
     }
   }
 
-  /**
-   * Register the stylesheets for the admin area.
-   *
-   * @since    1.0.0
-   */
   public function enqueue_styles() {
-    wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/jcg-plugins-admin.css', array(), $this->version, 'all' );
+    wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/jcg-plugins-admin.min.css', array(), $this->version, 'all' );
   }
 
   public function enqueue_login_styles() {
-    wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/jcg-plugins-login.css', array(), $this->version, 'all' );
+    wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/jcg-plugins-login.min.css', array(), $this->version, 'all' );
   }
 
-  /**
-   * Register the JavaScript for the admin area.
-   *
-   * @since    1.0.0
-   */
   public function enqueue_scripts($page) {
     wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/jcg-plugins-admin.js', array( 'jquery' ), $this->version, 'all' );
 
@@ -103,5 +54,4 @@ class JCG_Plugins_Admin {
       wp_enqueue_script( $this->plugin_name . '-upload', plugin_dir_url(__FILE__) . 'js/jcg-plugins-upload.js', array( 'jquery' ), $this->version, 'all' );
     }
   }
-
 }
